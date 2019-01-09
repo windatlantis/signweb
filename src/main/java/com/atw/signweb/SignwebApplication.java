5eeecf0fbc81ad50a9091d3e859edfc35830d447
windAtlantis <ufofre@gmail.com>
2019-01-09 21:45:28 +0800

    DEV:init

---
+++ src/main/java/com/atw/signweb/SignwebApplication.java
@@ -0,0 +1,14 @@
+package com.atw.signweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignwebApplication.class, args);
+	}
+
+}
+
